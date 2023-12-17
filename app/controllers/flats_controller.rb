55f26b74f1e5eeaf085662746b1927c1b0e18e6a
Fabien Meuret <fmeuret@gmail.com>
2023-12-17 12:24:10 +0100

    update view/create/destroy

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -25,9 +25,17 @@ class FlatsController < ApplicationController
   end
 
   def update
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat), notice: "Flat correctly updated!"
+    else
+      redirect_to root_path, alert: @flat.errors.full_messages.join(", ")
+    end
   end
 
   def destroy
+    if @flat.destroy
+      redirect_to root_path, notice: "Flat correctly deleted!"
+    end
   end
 
   private
